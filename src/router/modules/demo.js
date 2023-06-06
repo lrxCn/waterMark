d1bbd3573ca22eb72295c07a0b8ce8d2dd9432dc
lrx.cn <lrx_cn@163.com>
2023-06-06 18:51:19 +0800

    init

--- src/router/modules/demo.js
+++ src/router/modules/demo.js
@@ -5,11 +5,6 @@ export default [
     component: () => import('@/views/HomeView.vue'),
   },
   {
-    path: '/pinia',
-    name: 'pinia',
-    component: () => import('@/views/PiniaView.vue'),
-  },
-  {
     path: '/:w+',
     component: () => import('@/views/NotFound404.vue'),
   },
